0e3ad8658f0df0e9a64fd19474d558ddb2260112
Rosh <roshjingel@gmail.com>
2025-08-29 00:48:49 +0800

    UPDATED

--- script.js
+++ script.js
@@ -8,19 +8,15 @@ copyBtn = document.querySelector(".copy");
 function fetchRequest(file, formData) {
 infoText.innerText = "Scanning QR Code...";
 fetch("http://api.qrserver.com/v1/read-qr-code/", {
-    method: "POST",
-    body: formData,
+    method: "POST", body: formData,
 })
-    .then((res) => res.json())
-    .then((result) => {
+    .then(res => res.json())
+    .then(result => {
         result = result[0].symbol[0].data;
-        infoText.innerText = result
-            ? "Upload QR Code to Scan"
-            : "Couldn't scan QR Code";
+        infoText.innerText = result ? "Upload QR Code to Scan" : "Couldn't scan QR Code";
         if (!result) return;
-        document.querySelector("textarea").innerText = result;
-        form.querySelector("img").src =
-            URL.createObjectURL(file);
+        wrapper.querySelector("textarea").innerText = result;
+        form.querySelector("img").src = URL.createObjectURL(file);
         wrapper.classList.add("active");
     })
     .catch(() => {
@@ -28,22 +24,21 @@ fetch("http://api.qrserver.com/v1/read-qr-code/", {
     });
 }
 
-fileInp.addEventListener("change", async (e) => {
+fileInp.addEventListener("change", e => {
 let file = e.target.files[0];
-if (!file) return;
+if(!file) return;
 let formData = new FormData();
 formData.append("file", file);
 fetchRequest(file, formData);
 });
 
 copyBtn.addEventListener("click", () => {
-let text = document.querySelector("textarea").textContent;
+let text = wrapper.querySelector("textarea").textContent;
 navigator.clipboard.writeText(text);
 });
 
 form.addEventListener("click", () => fileInp.click());
-closeBtn.addEventListener("click", () =>
-wrapper.classList.remove("active")
+closeBtn.addEventListener("click", () => wrapper.classList.remove("active")
 );
 
 tippy('#About', {
